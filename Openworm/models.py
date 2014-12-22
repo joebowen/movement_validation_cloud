8b9e22ab9cf52ab3348e2216101bad6cf999e756
Joe Bowen <joe.bowen@toptal.com>
2014-12-22 09:35:14 -0700

    Initial commit

--- Openworm/models.py
+++ Openworm/models.py
@@ -6,7 +6,6 @@ LEXERS = [item for item in get_all_lexers() if item[1]]
 LANGUAGE_CHOICES = sorted([(item[1][0], item[0]) for item in LEXERS])
 STYLE_CHOICES = sorted((item, item) for item in get_all_styles())
 
-
 class Openworm(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=100, blank=True, default='')
@@ -15,5 +14,19 @@ class Openworm(models.Model):
     language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
     style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
 
-    class Meta:
-        ordering = ('created',)
+class Book(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class Article(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class BookReview(models.Model):
+    article = models.ForeignKey(Article)
+    book = models.ForeignKey(Book)
+    title = models.CharField(max_length=100, blank=True, default='')
+    code = models.TextField()
+    linenos = models.BooleanField(default=False)
+    language = models.CharField(choices=LANGUAGE_CHOICES, default='python', max_length=100)
+    style = models.CharField(choices=STYLE_CHOICES, default='friendly', max_length=100)
