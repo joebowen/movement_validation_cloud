c112bf2e33e91944def01bb25618d4171438a277
Joe Bowen <joe.bowen@toptal.com>
2015-01-06 06:29:10 -0700

    Fixing issues with video uploading.

--- Openworm/serializers.py
+++ Openworm/serializers.py
@@ -118,13 +118,13 @@ class PlatefeatureSerializer(serializers.ModelSerializer):
 
 class PlaterawvideoSerializer(serializers.ModelSerializer):
     #owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
-    platekey_id = serializers.PrimaryKeyRelatedField(queryset=Plate.objects.all())
+    platekey = serializers.PrimaryKeyRelatedField(queryset=Plate.objects.all())
     #videofile = forms.URLField(widget=S3DirectWidget(dest='destination_key_from_settings'))
     #videometadatakey = serializers.PrimaryKeyRelatedField(queryset=Videoattributes.objects.all())
 
     class Meta:
         model = Platerawvideo
-        fields = ('id', 'timestamp', 'name', 'description', 'title', 'shorttitle', 'videofileurl','fps', 'numframes', 'width', 'height', 'micronsperpixel', 'platekey_id')
+        fields = ('id', 'timestamp', 'name', 'description', 'title', 'shorttitle', 'videofileurl','fps', 'numframes', 'width', 'height', 'micronsperpixel', 'platekey')
         
 class PlatewireframevideoSerializer(serializers.ModelSerializer):
     #owner = serializers.PrimaryKeyRelatedField(queryset=User.objects.all())
