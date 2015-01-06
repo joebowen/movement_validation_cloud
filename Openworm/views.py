a88d98306e14e0c130b4d7118c596e62d56d681e
Joe Bowen <joe.bowen@toptal.com>
2015-01-06 03:57:58 -0700

    Fixing issues with video uploading.

--- Openworm/views.py
+++ Openworm/views.py
@@ -81,7 +81,7 @@ def handle_uploaded_item(request, model, post):
             k.key = str(time.time()) + "." + request.FILES['file'].name
             k.set_contents_from_file(request.FILES['file'])
             setattr(new_model, key, k.key)
-        if "key" not in key:
+        elif "key" not in key:
             setattr(new_model, key, value)
         else:
             app = get_app('Openworm')
